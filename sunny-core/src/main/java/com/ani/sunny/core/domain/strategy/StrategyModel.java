45d2d0d3674679560086f64b53a8abf7abc788ae
wyf <1336874736@qq.com>
2017-03-08 10:59:11 +0800

    remove aniService

--- sunny-core/src/main/java/com/ani/sunny/core/domain/strategy/StrategyModel.java
+++ sunny-core/src/main/java/com/ani/sunny/core/domain/strategy/StrategyModel.java
@@ -2,6 +2,7 @@ package com.ani.sunny.core.domain.strategy;
 
 import com.ani.sunny.core.domain.AbstractDomain;
 import com.ani.sunny.core.domain.user.User;
+import org.springframework.beans.factory.annotation.Configurable;
 
 import java.util.Date;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 /**
  * Created by wyf on 17-3-6.
  */
+@Configurable
 public class StrategyModel extends AbstractDomain {
     private static final long serialVersionUID = 7079048409075935101L;
 
@@ -21,4 +23,31 @@ public class StrategyModel extends AbstractDomain {
     public String[] repeatWeek;
     public User owner;
     public List<DeviceFeatureInstance> deviceFeatureInstanceList;
+
+    public StrategyModel(){}
+
+    public StrategyModel(String strategyId, String strategyName,
+                    String description, User owner,
+                    Date startTime, String[] repeatWeek,
+                    boolean isRepeat, boolean isScheduleNow,
+                    List<DeviceFeatureInstance> deviceFeatureInstanceList) {
+        this.strategyId = strategyId;
+        this.strategyName = strategyName;
+        this.description = description;
+        this.owner = owner;
+        this.startTime = startTime;
+        this.repeatWeek = repeatWeek;
+        this.isRepeat = isRepeat;
+        this.isScheduleNow = isScheduleNow;
+        this.deviceFeatureInstanceList = deviceFeatureInstanceList;
+    }
+
+    @Override
+    public String toString() {
+        return "Strategy{" +
+                "description='" + description + '\'' +
+                ", strategyName='" + strategyName + '\'' +
+                '}';
+    }
+
 }
